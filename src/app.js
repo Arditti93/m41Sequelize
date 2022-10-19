674d7c4defb64a82583f72b2d2ba13307561a1ae
Arditti93 <alex.arditti@hotmail.com>
2022-10-19 12:05:02 +0100

    app finished

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const yargs = require('yargs')
 const { sequelize } = require("./db/connection");
+const { createMovie, readMovies } = require('./movie/movieFunctions')
 
 
 const app = async (yargsObject) => {
@@ -7,10 +8,33 @@ const app = async (yargsObject) => {
         await sequelize.sync();
 
         if (yargsObject.create) {
-            //
+            await createMovie({ title: yargsObject.title, actor: yargsObject.actor })
+            let output = {}
+            let table = await readMovies()
+            for (let movie of table) {
+                output.id = movie.id
+                output.title = movie.title
+                output.actor = movie.actor
+                console.log(output)
+            }
         } else if (yargsObject.read) {
-            //
-        } else {
+            let output = {}
+            let table = await readMovies({ [yargsObject.key] : yargsObject.value})
+                output.id = table.id
+                output.title = table.title
+                output.actor = table.actor
+                console.log(output)
+        } else if (yargsObject.readAll) {
+            let output = {}
+            let table = await readMovies()
+            for (let movie of table) {
+                output.id = movie.id
+                output.title = movie.title
+                output.actor = movie.actor
+                console.log(output)
+            }
+        }
+        else {
             console.log("incorrect command")
         }
 
